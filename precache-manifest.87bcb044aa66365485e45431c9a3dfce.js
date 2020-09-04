b87f878e16d3fba2592591e62c8299e12a13f6f7
Your name here <your_email@example.com>
2020-09-04 14:34:50 -0700

    Updates

---
+++ precache-manifest.87bcb044aa66365485e45431c9a3dfce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82de59a8e7ce7d9388721e58042ccba",
+    "url": "/Chahaat/index.html"
+  },
+  {
+    "revision": "d38ffbef92bbf7130791",
+    "url": "/Chahaat/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "871c1c25a538184c219d",
+    "url": "/Chahaat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d38ffbef92bbf7130791",
+    "url": "/Chahaat/static/js/2.d1400638.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chahaat/static/js/2.d1400638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "871c1c25a538184c219d",
+    "url": "/Chahaat/static/js/main.ac467e74.chunk.js"
+  },
+  {
+    "revision": "b7571842e2a55c296690",
+    "url": "/Chahaat/static/js/runtime-main.ffe6ebfa.js"
+  }
+]);
